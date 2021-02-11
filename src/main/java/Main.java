4fb9b5d74fd81fff766899856d46c36a58b85c38
rfozdemir <furkanozdemir@furkans-mbp.myfiosgateway.com>
2021-02-10 22:09:01 -0500

    second commit

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -45,7 +45,7 @@ public class Main {
         System.out.println();
         System.out.println("======================================================================");
     }
-
+//commit
     public static void sortList(List<? extends Theatre.Seat> list) {
         for(int i=0; i<list.size() -1; i++) {
             for(int j=i+1; j<list.size(); j++) {
